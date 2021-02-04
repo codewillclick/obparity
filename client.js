3b50119a455e0038d577cc5a38eb5bca814509c0
cesar <ca2longoria@gmail.com>
2021-02-03 18:54:05 -0600

    code is messier, but declaring client methods in the client code is no longer a thing

--- client.js
+++ client.js
@@ -1,59 +1,71 @@
 
-function ParityObject(url,methods) {
+var ParityObject = class ParityObject {
 	
-	let baseMethods = [
-		'ping'
-	]
-	methods = methods ?
-		new Set(baseMethods.concat(methods)) :
-		new Set(baseMethods)
+	constructor(url,methods) {
 	
-	Object.defineProperties(this,{
-		fetchUrl: {
-			get:() => url,
-			enumerable:false
-		},
-		methods: {
-			get:() => methods,
-			enumerable:false,
-			configurable:true
-		}
-	})
-	
-	return new Proxy(this,{
-		// I guess assume all gets are async?
-		// I guess that makes sense, since anything here should be communicating
-		// with its paired server-side instance.
-		get: (function (target,prop,receiver) {
-			if (this.methods.has(prop)) {
-				//let funcArgs = Array.from(arguments)
-				let f = async function() {
-					let x = await fetch(this.fetchUrl,{
-						method:'POST',
-						headers: {
-							'content-type':'application/json'
-						},
-						body:JSON.stringify({
-							type:'call',
-							method:prop,
-							args:[...arguments]
-						})
-					})
-					// NOTE: See here?  Assumes a json-parsable return value.
-					return x.json()
-				}
-				return f
+		let baseMethods = $ParityObject_methods$
+		methods = methods ?
+			baseMethods.concat(methods) :
+			baseMethods
+		methods = new Set(methods.filter(a=>a))
+		
+		Object.defineProperties(this,{
+			fetchUrl: {
+				get:() => url,
+				enumerable:false
+			},
+			methods: {
+				get:() => methods,
+				enumerable:false,
+				configurable:true
 			}
-			else
-				return Reflect.get(...arguments)
-		}).bind(this)
-	})
+		})
+		
+		return new Proxy(this,{
+			// I guess assume all gets are async?
+			// I guess that makes sense, since anything here should be communicating
+			// with its paired server-side instance.
+			get: (function (target,prop,receiver) {
+				// Either the methods Set has the prop being asked for, at which point a
+				// function is returned which's call will be mirrored on the server, or
+				// Reflect.get() proceed's with the property's default behavior.
+				if (this.methods.has(prop)) {
+					// f here is the function made available to the client by the Proxy.
+					// So *its* arguments are what pass into the server's paired object's
+					// handleClientRequest() method.
+					
+					// TODO: Put f somewhere it doesn't have to be re-rendered every time
+					// a method property is referenced.
+					let f = async function() {
+						let x = await fetch(this.fetchUrl,{
+							method:'POST',
+							headers: {
+								'content-type':'application/json'
+							},
+							body:JSON.stringify({
+								type:'call',
+								method:prop,
+								args:[...arguments]
+							})
+						})
+						// ASSUMES: Return value is json-parsable.
+						return x.json()
+					}
+					return f
+				}
+				else
+					return Reflect.get(...arguments)
+			}).bind(this)
+		})
+	}
 }
 
 // TODO: Remove this once extendability is proven.
-function P2(url,methods) {
-	let px = new ParityObject(url,['pong'])
-
-	return px
+//function P2(url,methods) {
+var P2 = class P2 extends ParityObject {
+	constructor(url,methods) {
+		// NOTE: No longer *looks* hacky, but the implementation's a little grody.
+		return super(url,$P2_methods$.concat(methods))
+	}
 }
 
--- index.js
+++ index.js
@@ -14,19 +14,76 @@ function promitate(mod) {
 }
 fs.p = promitate(fs)
 
+//console.log('Parity be what', typeof ParityObject)
 class ParityObject {
 	
 	constructor(url) {
-		// I guess... allow this to be overridden if needed?
-		this.clientSourcePath = './client.js'
+		// Some overhead...
+		this._constructorUpdate()
 		
+		// Client source path is the javascript file containing the matching
+		// client-side class.
+		this.clientSourcePath = null
+		this.setClientSourcePath()
+		
+		// This property is meant to represent the url being listened for which's
+		// GET should return getClientSource(), and which's POST should return
+		// handleClientRequest(res.body).
 		this.fetchUrl = null
 		this.setClientFetchUrl(url)
 	}
 	
+	// Private methods meant to be run internally.
+	
+	_constructorUpdate() {
+		// Store top-level class names in a table for various computations.
+		if (!ParityObject._classNames.has(this.constructor.name)) {
+			// Add class name to this table.
+			ParityObject._classNames.add(this.constructor.name)
+			// Recompute the regex used to check for 
+			this._regexReplaceRecompute(
+				ParityObject._classRegex,
+				ParityObject._regexAppends,
+				ParityObject._classNames.values())
+			console.log('classNames  ',ParityObject._classNames)
+			console.log('classRegex  ',ParityObject._classRegex)
+			console.log('regexAppends',ParityObject._regexAppends)
+			// Static client methods table... is this a hack?
+			// WARNING: What if getClientMethods() is dynamic, though...?
+			//   Either way, a static table of something that is returned with a class
+			//   method implementation is wholly inelegant.  But for now it works!
+			//   I'll leave it for a refactor.
+			ParityObject._methodTable[this.constructor.name] = this.getClientMethods()
+			console.log('methodTable',ParityObject._methodTable)
+		}
+	}
+	_regexReplaceRecompute(table,keys,vals) {
+		// Per regex append string, recompute regex to match against when replacing
+		// strings in client source.
+		for (let k of keys) {
+			table[k] = new RegExp(
+				`\\$(${Array.from(vals).join('|')})_(${k})\\$`,
+				'g') // matchAll() is being used, so global it is
+		}
+	}
+	
+	// Methods getting and setting various object properties.
+	
 	setClientFetchUrl(url) {
 		this.fetchUrl = url
 	}
+	
+	setClientSourcePath(url) {
+		// Meant to be overridden by extending classes.
+		url = url ? url : 'client.js'
+		this.clientSourcePath = url
+	}
+	
+	getClientMethods() {
+		return ['ping','debug']
+	}
+	
+	// Methods interacting with the client-side object.
 	
 	async handleClientRequest(ob) {
 		if (ob.type === 'call') {
@@ -45,13 +102,41 @@ class ParityObject {
 	
 	async getClientSource() {
 		try {
-			return await fs.p.readFile(
+			// clientSourcePath searches from the location of index.js.
+			
+			// TODO: Figure out what happens when this is called from a different
+			//   directory.  For example, a separate parity object module extending
+			//   ParityObject.  Will it reference __dirname from its own location?
+			//   Or will it reference obparity's directory?
+			let source = await fs.p.readFile(
 				path.resolve(__dirname,this.clientSourcePath))
+			
+			// Determine file prefixes based on presence of certain variable names.
+			
+			// NOTE: Let's try this as an anti-hack.
+			// HACK: It's a hack.  If I want to do this, there needs to be some kind
+			//   of globally accessible var listing out all the replace-able strings.
+			let r,x = ParityObject._classRegex['methods']
+			let prefixes=[], added=new Set()
+			for (let m of new String(source).matchAll(x)) {
+				// Prefix the source file with... jank.
+				if (added.has(m[1]))
+					continue
+				let s = `\$${m[1]}_methods\$`,
+				    v = JSON.stringify(ParityObject._methodTable[m[1]])
+				prefixes.push(`var ${s} = ${s} || ${v};`)
+				added.add(m[1])
+			}
+			if (prefixes.length)
+				source = prefixes.join('\n') + source
+			return source
 		} catch(e) {
 			if (true) // check against some specific error type
 				throw e
 		}
 	}
+	
+	// Methods accessible to the client object.
 	
 	async ping(...r) {
 		return {
@@ -62,13 +147,41 @@ class ParityObject {
 			srclen:this.renderedSource && this.renderedSource.length
 		}
 	}
+	
+	async debug(...r) {
+		return {
+			ParityObject_global: {
+				classNames:Array.from(ParityObject._classNames),
+				classRegex:Object.fromEntries(Object.entries(
+					ParityObject._classRegex).map(r=>[r[0],''+r[1]])),
+				regexAppends:ParityObject._regexAppends,
+				methodTable:ParityObject._methodTable
+			}
+		}
+	}
 }
 M.ParityObject = ParityObject
+// NOTE: Consider just leaving these as local variables, if they're meant to be
+//   singletons anyway.  Get rid of some of the 'ParityObject'* clutter.
+// Set of class names for classes extending ParityObject.
+ParityObject._classNames = new Set()
+// Regex table storing regexes to check for use of special $vars$ in source.
+ParityObject._classRegex = {}
+// Suffixes to append to regex checks.
+ParityObject._regexAppends = ['methods']
+// Static table of methods returned by getClientMethods(), with each new class.
+ParityObject._methodTable = {}
 
 // This is more a test of extendability practice than anything.
+// TODO: Remove this once extendability is proven.
 class P2 extends ParityObject {
 	constructor(...r) {
 		super(...r)
+	}
+	
+	getClientMethods() {
+		//return super.getClientMethods().concat(['pong'])
+		return ['pong']
 	}
 	
 	async pong(...r) {
